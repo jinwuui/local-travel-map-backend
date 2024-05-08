6a932004c591bb919af250c6d259dfebc38ccb83
박진우 <jinwoo@bagjin-uui-noteubug.local>
2024-05-08 09:38:12 +0900

    feat: connect api layer with frontend

--- src/routes/location.js
+++ src/routes/location.js
@@ -4,7 +4,14 @@ const router = Router();
 
 const locationController = require("../controllers/location");
 
-router.get("/create", async (req, res) => {
+router.get("/", async (req, res) => {
+  console.log("get");
+  locationController.createLocation();
+  res.send("create location");
+});
+
+router.post("/", async (req, res) => {
+  console.log("post");
   locationController.createLocation();
   res.send("create location");
 });
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const router = Router();
 
-router.use("/api/location", require("./location"));
+router.use("/api/locations", require("./location"));
 
 module.exports = router;

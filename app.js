da8b390b01a6c3b365d14266297e823a5effa2ec
jinwuui <jinwuui@gmail.com>
2024-05-21 14:16:43 +0900

    feat: add model Category and PlaceCategories relation table(model)

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const router = require("./src/routes/router.js");
 const sequelize = require("./src/config/database.js");
-const { Place, Photo } = require("./src/models");
 
 const app = express();
 
